414fdf58279227d1de577c49d580d42d0d1dff06
brochj <brochj@gmail.com>
2022-06-05 18:55:47 -0300

    Add option to choose the cache folder

---
+++ html_downloaders/requests_cache_html_downloader.py
@@ -0,0 +1,29 @@
+import requests_cache
+from core.html_downloader import HTMLDownloader
+
+
+class RequestsCacheHTMLDownloader(HTMLDownloader):
+    def __init__(self, configs, cache_filename: str) -> None:
+        self.configs = configs
+        self.cache_filename = cache_filename
+        self.cache_directory = self.configs.get("cacheDir") + cache_filename
+
+    def get_html(self, url: str) -> str:
+        session = requests_cache.CachedSession(self.cache_directory)
+        html_downloaded = session.get(url)
+        return html_downloaded.text
+
+
+def __main():
+    # Just for testing
+    import lib.config_section_reader as cfg
+
+    requests = cfg.ConfigSectionReader(
+        "./configs/html_downloaders.ini", "RequestsCache"
+    )
+    html_downloader = RequestsCacheHTMLDownloader(requests.configs, "tests")
+    html_downloader.get_html("https://google.com")
+
+
+if __name__ == "__main__":
+    __main()
--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 # Just for testing
 import lib.config_section_reader as cfg
-from html_downloaders.requests_html_downloader import Requests
+from html_downloaders.requests_cache_html_downloader import RequestsCacheHTMLDownloader
+from html_downloaders.requests_html_downloader import RequestsHTMLDownloader
 from html_parsers.beautiful_soup_html_parser import BeautifulSoupHTMLParser
-import scrapers
+from scrapers.webpage_scraper import WebpageScraper
 
-requests = cfg.ConfigSectionReader("./configs/html_downloaders.ini", "Requests")
-html_downloader = Requests(requests.configs)
-html_downloaded = html_downloader.get_html(
-    # "https://download-cursos.netlify.app/posts/red-team-operator"
-    "https://compartilhandobr.com/posts/red-team-operator"
-)
+URL = "https://compartilhandobr.com/posts/red-team-operator"
+URL = "https://download-cursos.netlify.app/posts/red-team-operator"
+
+
+# requests = cfg.ConfigSectionReader("./configs/html_downloaders.ini", "Requests")
+# html_downloader = RequestsHTMLDownloader(requests.configs)
+# html_downloaded = html_downloader.get_html(URL)
+
+# With Cache Feature
+requests = cfg.ConfigSectionReader("./configs/html_downloaders.ini", "RequestsCache")
+html_downloader = RequestsCacheHTMLDownloader(requests.configs, "webpage")
+html_downloaded = html_downloader.get_html(URL)
+
+
 html_parser = BeautifulSoupHTMLParser()
 parsed_html = html_parser.parse_html(html_downloaded)
 
 # person_scraper = scrapers.PersonScraper(parsed_html)
 # myperson = person_scraper.scrape()
 
-webpage_scraper = scrapers.WebpageScraper(parsed_html)
-myperson = webpage_scraper.scrape()
-# print(myperson)
+webpage_scraper = WebpageScraper(parsed_html)
+page_model = webpage_scraper.scrape()
+# print(page_model.html)
